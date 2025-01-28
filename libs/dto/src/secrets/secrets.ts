80e42afb88e24a87cbb08c0ffb55e8554a752825
turovskiy <alexturik@gmail.com>
2025-01-28 20:18:43 +0200

    init keys

--- libs/dto/src/secrets/secrets.ts
+++ libs/dto/src/secrets/secrets.ts
@@ -1,7 +1,8 @@
 import { idSchema } from "@reactive-resume/schema";
 import { z } from "zod";
 
-export const secretsSchema = z.object({
+export const secretsSchema = z.object
+({
   id: idSchema,
   password: z.string().nullable(),
   lastSignedIn: z.date().nullable(),
@@ -11,4 +12,4 @@ export const secretsSchema = z.object({
   refreshToken: z.string().nullable(),
   resetToken: z.string().nullable(),
   userId: idSchema,
-});
+})
